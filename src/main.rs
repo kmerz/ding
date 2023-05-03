59b1be30831f40b1d7facf1208ef251677821e77
Konrad Merz <konrad@graylog.com>
2023-05-03 14:18:07 +0200

    Clean up and add reading string

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,18 @@
-use chess::{Board};
+use chess::{Game, Square, ChessMove, Board};
+
+mod ui;
 
 fn main() {
-    let board = Board::default();
-    print_board(&board);
-}
+    let mut game = Game::new();
 
-fn print_board(board: &Board) {
-    //  A B C D E F G H
-    //8 ♚ . . . . . ♖ .
-    //7 . . . . ♖ . . .
-    //6 . . . . . . . .
-    //5 . . . . . . . .
-    //4 . . . . . . . .
-    //3 . . . . . . . .
-    //2 . . . . . . . .
-    //1 . . . . . . . .
-    
-    print!("  A B C D E F G H\n8 ");
+    while game.result().is_none() {
+        ui::print_board(&game.current_position());
 
-    const ROWS: i32 = 8;
-    let mut counter = 1; 
-    let board_fen_string = board.to_string();
-    let positions = board_fen_string.split(" ").next().unwrap();
-    for position in positions.chars() {
-        if position.is_numeric() {
-            let empty_fields = (position.to_string()).parse::<i16>().unwrap();
-            for _ in 0..empty_fields {
-                print!(". ")
-            }
-        } else if position == '/' {
-            let row = ROWS - counter;
-            print!("\n{} ", row); 
-            counter = counter + 1;
-        } else {
-            let fig = lookup_figure(position);
-            print!("{} ", fig);
-        }
-    }
+        print!("> ");
+        let mut input = ui::read_str();
+        println!("You typed: {}", input);
 
-    print!("\n");
-}
-
-fn lookup_figure(current: char) -> &'static str {
-    match current {
-      'K' => "♔",
-      'Q' => "♕",
-      'R' => "♖",
-      'B' => "♗",
-      'N' => "♘",
-      'P' => "♙",
-      'k' => "♚",
-      'q' => "♛",
-      'r' => "♜",
-      'b' => "♝",
-      'n' => "♞",
-      'p' => "♟︎",
-      _ => "."
     }
 }
+
+
